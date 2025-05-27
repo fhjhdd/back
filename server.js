92c80f144017d6fbb30afb3cbfe1849b6427c86a
adarsh <adarshsahu2510@gmail.com>
2025-05-27 23:10:10 +0530

    .

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "insurance-portal-frontend.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://www.myteamboost.com/", credentials: true }));
 // app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
